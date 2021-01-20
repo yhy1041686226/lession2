82103b114f10d4247bcd2dac108537649b87b1a5
yanghy <1041686226@qq.com>
2021-01-20 11:14:03 +0800

    lession2项目提交代码到git仓库

---
+++ src/main/java/com/example/lession2/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.lession2.controller;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @RequestMapping("/test1")
+    @ResponseBody
+    public String  test1(){
+        return "hello test1";
+     }
+}
